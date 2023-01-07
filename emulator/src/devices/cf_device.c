dd7b39ffa151f63ee54182212ba4f0cdcea640cb
Jan Królikowski <yanekk86@gmail.com>
2023-01-07 11:03:42 +0100

    Replace conditional ifs with switch

--- emulator/src/devices/cf_device.c
+++ emulator/src/devices/cf_device.c
@@ -78,8 +78,10 @@ static void destroyCompactFlashDevice(HBC56Device *device)
 static uint8_t readCompactFlashDevice(HBC56Device* device, uint16_t addr, uint8_t *val, uint8_t dbg)
 {
   const CompactFlashDevice* cfDevice = getCompactFlashDevice(device);
-
-  if (addr == cfDevice->startAddr + CF_STAT) {
+  uint8_t offset = addr - cfDevice->startAddr;
+  
+  switch(offset) {
+    case CF_STAT:
     *val |= CF_Read_Status_Error(cfDevice->compactFlash) << 0;
     *val |= CF_Read_Status_CorrectableDataError(cfDevice->compactFlash) << 2;
     *val |= CF_Read_Status_DataRequest(cfDevice->compactFlash) << 3;
@@ -87,19 +89,19 @@ static uint8_t readCompactFlashDevice(HBC56Device* device, uint16_t addr, uint8_
     *val |= CF_Read_Status_DriveWriteFault(cfDevice->compactFlash) << 5;
     *val |= CF_Read_Status_Ready(cfDevice->compactFlash) << 6;
     *val |= CF_Read_Status_Busy(cfDevice->compactFlash) << 7;
-    return 1;
-  }
-  if (addr == cfDevice->startAddr + CF_ERR) {
+    break;
+
+    case CF_ERR:
     *val |= CF_Read_Error_BadBlock(cfDevice->compactFlash) << 7;
     *val |= CF_Read_Error_UncorrectableError(cfDevice->compactFlash) << 6;
     *val |= CF_Read_Error_InvalidSector(cfDevice->compactFlash) << 4;
     *val |= CF_Read_Error_InvalidCommand(cfDevice->compactFlash) << 2;
     *val |= CF_Read_Error_GeneralError(cfDevice->compactFlash) << 0;
-    return 1;
-  }
-  if(addr == cfDevice->startAddr + CF_DATA) {
+    break;
+
+    case CF_DATA:
     *val = CF_Read_Data(cfDevice->compactFlash);
-    return 1;
+    break;
   }
 
   return 1;
@@ -108,36 +110,44 @@ static uint8_t readCompactFlashDevice(HBC56Device* device, uint16_t addr, uint8_
 static uint8_t writeCompactFlashDevice(HBC56Device* device, uint16_t addr, uint8_t val)
 {
     const CompactFlashDevice* cfDevice = getCompactFlashDevice(device);
-
-    if(addr == cfDevice->startAddr + CF_SECCO) {
+    uint8_t offset = addr - cfDevice->startAddr;
+    switch(offset) {
+      case CF_SECCO:
       CF_Write_SectorCount(cfDevice->compactFlash, val);
-    }
-    if(addr == cfDevice->startAddr + CF_LBA0) {
+      break;
+
+      case CF_LBA0:
       CF_Write_SectorNumber(cfDevice->compactFlash,
         (CF_Read_SectorNumber(cfDevice->compactFlash) & 0xFFFFFF00) | val
       );
-    }
-    if(addr == cfDevice->startAddr + CF_LBA1) {
+      break;
+
+      case CF_LBA1:
       CF_Write_SectorNumber(cfDevice->compactFlash,
         (CF_Read_SectorNumber(cfDevice->compactFlash) & 0xFFFF00FF) | (val << 8)
       );
-    }
-    if(addr == cfDevice->startAddr + CF_LBA2) {
+      break;
+
+      case CF_LBA2:
       CF_Write_SectorNumber(cfDevice->compactFlash,
         (CF_Read_SectorNumber(cfDevice->compactFlash) & 0xFF00FFFF) | (val << 16)
       );
-    }
-    if(addr == cfDevice->startAddr + CF_LBA3) {
+      break;
+
+      case CF_LBA3:
       CF_Write_SectorNumber(cfDevice->compactFlash,
         (CF_Read_SectorNumber(cfDevice->compactFlash) & 0x00FFFFFF) | ((val << 24) & 0x1F000000)
       );
-    }
-    if(addr == cfDevice->startAddr + CF_CMD) {
+      break;
+
+      case CF_CMD:
       switch(val) {
         case CF_Command_ReadSectors:
         CF_Write_Command_ReadSectors(cfDevice->compactFlash);
-        return 1;
+        break;
       }
+      break;
+
     }
     return 1;
 }
