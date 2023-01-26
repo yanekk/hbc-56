c52b1d15faa36414ab5f8d11e8ed64be42b7675f
Jan Królikowski <yanekk86@gmail.com>
2023-01-26 08:31:10 +0100

    Handle ACIA write

--- emulator/test/devices/test_acia_device.c
+++ emulator/test/devices/test_acia_device.c
@@ -78,14 +78,37 @@ void test_writeDevice_returnsOneOnAddressInsideScope(void)
     destroyAciaDevice(&testDevice);
 }
 
+char buffer[64] = {0};
+char* bufferPtr = buffer;
+
+void printToBuffer(uint8_t c) {
+    *bufferPtr++ = c;
+}
+
+void test_writeDevice_bytesAreTransmitted(void)
+{
+    // arrange
+    buffer[0] = 0;
+    bufferPtr = buffer;
+    HBC56Device testDevice = createAciaDeviceWithOutput(BASE_ADDRESS, printToBuffer);
+    
+    // act
+    writeDevice(&testDevice, BASE_ADDRESS, 'a');
+    writeDevice(&testDevice, BASE_ADDRESS, 'b');
+    writeDevice(&testDevice, BASE_ADDRESS, 'c');
+    
+    TEST_CHECK(strcmp(buffer, 'abc') == 1);
+
+    // cleanup
+    destroyAciaDevice(&testDevice);
+}
+
 TEST_LIST = {
    { "test_createDevice_nameIsSet", test_createDevice_nameIsSet },
    { "test_readDevice_returnsZeroOnAddressOutsideOfScope", test_readDevice_returnsZeroOnAddressOutsideOfScope },
    { "test_readDevice_returnsOneOnAddressInsideScope", test_readDevice_returnsOneOnAddressInsideScope },
    { "test_writeDevice_returnsZeroOnAddressOutsideOfScope", test_writeDevice_returnsZeroOnAddressOutsideOfScope },
    { "test_writeDevice_returnsOneOnAddressInsideScope", test_writeDevice_returnsOneOnAddressInsideScope },
-
-//    { "test_writeDevice_returnsZeroOnAddressOutsideOfScope", test_writeDevice_returnsZeroOnAddressOutsideOfScope },
-//    { "test_writeDevice_returnsOneOnAddressInsideScope", test_writeDevice_returnsOneOnAddressInsideScope }, 
+   { "test_writeDevice_bytesAreTransmitted", test_writeDevice_bytesAreTransmitted },
    { NULL, NULL }     /* zeroed record marking the end of the list */
 };
