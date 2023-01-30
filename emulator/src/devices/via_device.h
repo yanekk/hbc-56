eac9b239126f7ea1de893fff90394cbe48cea8a1
Jan Królikowski <yanekk86@gmail.com>
2023-01-30 08:39:26 +0100

    e2e test for setting interrupt flag on timeout

--- emulator/src/devices/via_device.h
+++ emulator/src/devices/via_device.h
@@ -7,6 +7,22 @@
 extern "C" {
 #endif
 
+#define VIA_PB   0
+#define VIA_PA   1
+#define VIA_DDRB 2
+#define VIA_DDRA 3
+#define VIA_T1CL 4
+#define VIA_T1CH 5
+#define VIA_T1LL 6
+#define VIA_T1LH 7
+#define VIA_T2CL 8
+#define VIA_T2CH 9
+#define VIA_SR   10
+#define VIA_ACR  11
+#define VIA_PCR  12
+#define VIA_IFR  13
+#define VIA_IER  14
+
 HBC56Device createViaDevice(uint16_t baseAddr);
 void destroyViaDevice(HBC56Device* viaDevice);
 
--- emulator/test/devices/test_via_device.c
+++ emulator/test/devices/test_via_device.c
@@ -2,6 +2,7 @@
 #include "devices/via_device.h"
 
 #define BASE_ADDRESS 0x6000
+#define VIA_REG(reg) (BASE_ADDRESS+reg)
 
 void test_createDevice_nameIsSet(void)
 {
@@ -21,10 +22,10 @@ void test_readDevice_returnsZeroOnAddressOutsideOfScope(void)
     
     // act
     uint8_t value = 0;
-    uint8_t isHandled = readDevice(&testDevice, BASE_ADDRESS - 1, &value, true);
+    uint8_t isHandled = readDevice(&testDevice, VIA_REG(VIA_PB) - 1, &value, true);
     TEST_CHECK(isHandled == 0);
 
-    isHandled = readDevice(&testDevice, BASE_ADDRESS + 15, &value, true);
+    isHandled = readDevice(&testDevice, VIA_REG(VIA_IER) + 1, &value, true);
     TEST_CHECK(isHandled == 0);
 
     // assert
@@ -41,11 +42,11 @@ void test_readDevice_returnsOneOnAddressInsideScope(void)
     
     // act
     uint8_t value = 0;
-    uint8_t isHandled = readDevice(&testDevice, BASE_ADDRESS, &value, true);
+    uint8_t isHandled = readDevice(&testDevice, VIA_REG(VIA_PB), &value, true);
     TEST_CHECK(isHandled == 1);
 
 
-    isHandled = readDevice(&testDevice, BASE_ADDRESS+3, &value, true);
+    isHandled = readDevice(&testDevice, VIA_REG(VIA_DDRA), &value, true);
     TEST_CHECK(isHandled == 1);
 
     // cleanup
@@ -58,8 +59,8 @@ void test_writeDevice_returnsZeroOnAddressOutsideOfScope(void)
     HBC56Device testDevice = createViaDevice(BASE_ADDRESS);
     
     // act & assert
-    TEST_CHECK(writeDevice(&testDevice, BASE_ADDRESS-1, 0) == 0);
-    TEST_CHECK(writeDevice(&testDevice, BASE_ADDRESS+15, 0) == 0);
+    TEST_CHECK(writeDevice(&testDevice, VIA_REG(VIA_PB) - 1, 0) == 0);
+    TEST_CHECK(writeDevice(&testDevice, VIA_REG(VIA_IER) + 1, 0) == 0);
 
     // cleanup
     destroyDevice(&testDevice);
@@ -71,8 +72,41 @@ void test_writeDevice_returnsOneOnAddressInsideScope(void)
     HBC56Device testDevice = createViaDevice(BASE_ADDRESS);
     
     // act
-    TEST_CHECK(writeDevice(&testDevice, BASE_ADDRESS, 0) == 1);
+    TEST_CHECK(writeDevice(&testDevice, VIA_REG(VIA_PB), 0) == 1);
     TEST_CHECK(writeDevice(&testDevice, BASE_ADDRESS+3, 0) == 1);
+
+    // cleanup
+    destroyDevice(&testDevice);
+}
+
+uint8_t hbcInterruptCallCount = 0;
+void test_timer1_freeMode_noPB7Output(void)
+{
+    // arrange
+    uint8_t timerCycles = 16;
+    HBC56Device testDevice = createViaDevice(BASE_ADDRESS);
+    
+    // act
+    writeDevice(&testDevice, VIA_REG(VIA_ACR), VIA_ACR_TIMER1_CONTINOUS_MODE);
+    // enable continous mode with PB7 disabled
+
+    writeDevice(&testDevice, VIA_REG(VIA_IER), VIA_IER_SET | VIA_IER_TIMER1);
+    // enable Timer1 interrupt
+
+    writeDevice(&testDevice, VIA_REG(VIA_T1CL), timerCycles);
+    writeDevice(&testDevice, VIA_REG(VIA_T1CH), 0);
+    // counting starts on writing to VIA_T1CH
+
+    // cycle to reach the interrupt
+    tickDevice(&testDevice, 16, 0);
+
+    // assert
+    TEST_CHECK_(hbcInterruptCallCount == 1, "expected HBC interrupt to be called once, found %d", hbcInterruptCallCount);
+
+    uint8_t actualInterruptFlag;
+    readDevice(&readDevice, VIA_REG(VIA_IFR), &actualInterruptFlag, true);
+    TEST_CHECK_(actualInterruptFlag == VIA_IFR_ANY | VIA_IFR_TIMER1, 
+        "expected interrupt flag to be set to timer1, found %X", actualInterruptFlag);
 
     // cleanup
     destroyDevice(&testDevice);
